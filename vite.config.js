91736cd6103e8278bd57c856da644541f147ca6a
sujaytumu <sujaybabutumu@gmail.com>
2025-08-08 11:19:49 +0530

    fix: updated for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/vendor': {
+        target: 'https://yummy-food-app-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
